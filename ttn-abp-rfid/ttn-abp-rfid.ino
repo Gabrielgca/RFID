7da06136d7f42847a51da7c01a2305708d5a3ac6
THETNT\User <renatotbreis@gmail.com>
2020-06-01 11:39:42 -0300

    Código Definitivo (TTN ABP RFID)
    -> Versão mais estável (a outra eventualmente dava "FAILURE" em execução)
    -> Código organizado e comentado
    -> Algumas melhorias nos comentários e na função do led
    -> Nome definitivo referente à plataforma, modo de comunicação e projeto em si; "ttn-abp-rfid.ino"

--- ttn-abp-rfid/ttn-abp-rfid.ino
+++ ttn-abp-rfid/ttn-abp-rfid.ino
@@ -1,6 +1,6 @@
 //Bibliotecas
 #include <SPI.h>
-#include "MFRC522.h"
+#include <MFRC522.h>
 #include <lmic.h>
 #include <hal/hal.h>
 #include <SPI.h>
@@ -12,26 +12,26 @@ static const PROGMEM u1_t NWKSKEY [16] = {0x64, 0x9B, 0x3D, 0x56, 0x09, 0xD7, 0x
 static const u1_t PROGMEM APPSKEY [16] = {0x2E, 0x78, 0xED, 0x07, 0x2F, 0x9C, 0x83, 0x1A, 0x6A, 0x80, 0x71, 0x8E, 0xBD, 0x9C, 0xDA, 0x97};
 
 //device address de acordo com a TTN; este endereço é único por dispositivo
-static const u4_t DEVADDR = 0x2602137F;
+static const u4_t DEVADDR = 0x2602137F; // <-- Change this address for every node!
 
 //desabilitando modo OTAA; sem estas 3 linhas o código acusa erro
 void os_getArtEui (u1_t* buf) { }
 void os_getDevEui (u1_t* buf) { }
 void os_getDevKey (u1_t* buf) { }
 
-static uint8_t tagID[4] = "";						//variável que armazena o ID das tags lidas pelo RFID
-static osjob_t sendjob;									//variável usada pela LMIC para agendar pacotes
-static bool newCard = false;						//variável de controle usada para checar se há um cartão novo no leitor
+static uint8_t tagID [4] = "";						//variável que armazena o ID das tags lidas pelo RFID
+static osjob_t sendjob;										//variável usada pela LMIC para agendar pacotes
+bool isUplinkComplete = false;						//confirmação de uplink
 
 //intervalo entre envio de pacotes em segundos
 const unsigned TX_INTERVAL = 1;
 
 //pinagem referente ao LoRa
 const lmic_pinmap lmic_pins = {
-	.nss = 10,
-	.rxtx = LMIC_UNUSED_PIN,
-	.rst = 5,
-	.dio = {2, 3, LMIC_UNUSED_PIN},
+    .nss = 10,
+    .rxtx = LMIC_UNUSED_PIN,
+    .rst = 5,
+    .dio = {2, 3, LMIC_UNUSED_PIN},
 };
 
 //tratamento de eventos do LMIC
@@ -39,7 +39,6 @@ void onEvent (ev_t ev)
 {
 	Serial.print (os_getTime ());
 	Serial.print (": ");
-	
 	switch (ev)
 	{
 		case EV_SCAN_TIMEOUT:
@@ -68,7 +67,7 @@ void onEvent (ev_t ev)
 			break;
 			
 		case EV_RFU1:
-			Serial.println (F ("EV_RFU1"));
+			Serial.println (F("EV_RFU1"));
 			break;
 			
 		case EV_JOIN_FAILED:
@@ -81,7 +80,10 @@ void onEvent (ev_t ev)
 			
 		//uplink:
 		case EV_TXCOMPLETE:
-			Serial.println (F ("EV_TXCOMPLETE (includes waiting for RX windows)"));
+			//uplink realizado
+			Serial.println(F("EV_TXCOMPLETE (includes waiting for RX windows)"));
+			isUplinkComplete = true;
+			
 			if (LMIC.txrxFlags & TXRX_ACK)
 				Serial.println (F ("Received ack"));
 			if (LMIC.dataLen)
@@ -90,9 +92,9 @@ void onEvent (ev_t ev)
 				Serial.println (LMIC.dataLen);
 				Serial.println (F (" bytes of payload"));
 			}
+			
 			//agendar próxima transmissão
-			if (newCard == true)
-				os_setTimedCallback (&sendjob, os_getTime () + sec2osticks (TX_INTERVAL), do_send);
+			os_setTimedCallback (&sendjob, os_getTime () + sec2osticks (TX_INTERVAL), do_send);
 			break;
 			
 		case EV_LOST_TSYNC:
@@ -132,10 +134,9 @@ void do_send (osjob_t* j)
 		Serial.println (F ("OP_TXRXPEND, not sending"));
 	else
 	{
-		//prepara um pacote de uplink para a próxima janela de transmissão disponível
+		//prepara um pacote de uplink para a próxima janela de transmissão disponível sem confirmação
 		LMIC_setTxData2 (1, tagID, 4, 0);
 		Serial.println (F ("Packet queued"));
-		
 	}
 	//o próximo TX é agendado depois do evento TX_COMPLETE
 }
@@ -155,18 +156,18 @@ void setup ()
   Serial.println (F ("Starting..."));
   
 	//pullup; inicializando SS do LoRa e RFID e definindo ambos como saída para evitar interferência e vazamento de dados
-  pinMode (10, OUTPUT);
-  digitalWrite (10, HIGH);
+  pinMode (10,OUTPUT);
+  digitalWrite (10,HIGH);
   pinMode (4, OUTPUT);
   digitalWrite (4, HIGH);
-	
-	//inicializando o sistema LMIC / LoRa...
+
+  //inicializando o sistema LMIC / LoRa...
   os_init ();
   LMIC_reset ();
 	
 	//definições da LMIC
   #ifdef PROGMEM
-  //On AVR, these values are stored in flash and only copied to RAM
+  //on AVR, these values are stored in flash and only copied to RAM
   //once. Copy them to a temporary buffer here, LMIC_setSession will
   //copy them into a buffer of its own again.
   uint8_t appskey [sizeof (APPSKEY)];
@@ -175,76 +176,79 @@ void setup ()
   memcpy_P (nwkskey, NWKSKEY, sizeof (NWKSKEY));
   LMIC_setSession (0x1, DEVADDR, nwkskey, appskey);
   #else
-  // If not running an AVR with PROGMEM, just use the arrays directly
+  //if not running an AVR with PROGMEM, just use the arrays directly
   LMIC_setSession (0x1, DEVADDR, NWKSKEY, APPSKEY);
   #endif
 	
 	//sub banda do plano de frequências AU915
   #if defined(CFG_us915)
-    //NA-US channels 0-71 are configured automatically
-    //but only one group of 8 should (a subband) should be active
-    //TTN recommends the second sub band, 1 in a zero based count.
-    //https://github.com/TheThingsNetwork/gateway-conf/blob/master/US-global_conf.json
+    // NA-US channels 0-71 are configured automatically
+    // but only one group of 8 should (a subband) should be active
+    // TTN recommends the second sub band, 1 in a zero based count.
+    // https://github.com/TheThingsNetwork/gateway-conf/blob/master/US-global_conf.json
     LMIC_selectSubBand (1);
-  #endif
-	
-  //Disable link check validation
-  LMIC_setLinkCheckMode(0);
-	
+    
+    #endif
+
+  // Disable link check validation
+  LMIC_setLinkCheckMode (0);
+
   //configurando downlink para SF9 e uplink para SF7
   LMIC.dn2Dr = DR_SF9;
   LMIC_setDrTxpow (DR_SF7, 14);
 	//inicialização LMIC / LoRa concluída--
-  
+	
   SPI.begin ();                    									//inicializando comunicação SPI 
   RFID.PCD_Init ();                									//inicializando leitor RFID
-  pinMode (LED_GREEN, OUTPUT);   									  //inicializando pino verde no modo de saída
+  pinMode (LED_GREEN, OUTPUT);     									//inicializando pino verde no modo de saída
   RFID.PCD_SetAntennaGain (RFID.RxGain_max);				//aumenta o alcance da antena do leitor RFID para o máximo
 }
 
 void loop ()
-{
-	//rotina do módulo LoRa que envia e recebe dados
-	os_runloop_once ();
-  
-  //verifica se existe uma tag presente
-	if (!RFID.PICC_IsNewCardPresent ())
-	{
-		//se não, newCard recebe o valor falso e o loop volta para o começo
-		newCard = false;
-		return;
-	}
-	if (!RFID.PICC_ReadCardSerial ())
+{  
+	//verifica se tem uma tag no leitor RFID a cada 100 milissegundos
+  if (!RFID.PICC_IsNewCardPresent () || !RFID.PICC_ReadCardSerial ())
+  {
+    delay (100);
     return;
+  }
+  Serial.print("Nova Tag lida.\n");
 	
-	//caso contrário, newCard recebe o valor verdadeiro e o loop prossegue
-	newCard = true;
-		
   //pega o ID da tag e armazena na variável tagID; joga o ID no serial
   for (byte i = 0; i < RFID.uid.size; i++)
   {
     tagID [i] = RFID.uid.uidByte [i];
-    Serial.print (tagID [i], HEX);
+    Serial.print (tagID [i],HEX);
     Serial.print (" ");
   }
   Serial.print ("\n");
-	
-	//envia o ID para a TTN e aciona o led verde para confirmação
+  
+	//envia o ID para a TTN
   do_send (&sendjob);
-	tagIdentified ();
+  
+	//rotina do módulo LoRa que envia e recebe dados; executa o loop até os dados serem entregues
+  while (isUplinkComplete != true)
+		os_runloop_once ();
 	
-	//aguarda antes de voltar à rotina
-  delay(1000);
+  Serial.print ("Terminou loop.\n");
+	
+  isUplinkComplete = false;
+	
+	//aciona o led verde para confirmação e aguarda antes de voltar à rotina
+	tagIdentified (1, 500);
+  delay (500);
 }
 
-void tagIdentified ()
+/*função que ascende o led para confirmação
+	count = número de bips
+	ledDelay = tempo que fica aceso
+*/
+void tagIdentified (int count, int ledDelay)
 {
-  int count = 1; //definindo a quantidade de bips
   for (int j = 0; j < count; j++)
   {
     digitalWrite (LED_GREEN, HIGH);   
-    delay (500);
+    delay (ledDelay);
 		digitalWrite (LED_GREEN, LOW);
-		delay (500);
   }
 }
