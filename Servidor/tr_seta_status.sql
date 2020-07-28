318a32ffdc56f1f576775ec8888cc0e5101961d2
SauloSCorrea <65734343+SauloSCorrea@users.noreply.github.com>
2020-07-28 17:48:04 -0300

    Alterações nos scripts

--- Servidor/tr_seta_status.sql
+++ Servidor/tr_seta_status.sql
@@ -1,26 +1,50 @@
---- Trigger que seta o status da ocorrência (Entrada = 'E' ou Saída = 'S') 
---- de acordo com o status da última ocorrência de um determinado usuário.
---- Se a última ocorrência for entrada setar como saída e vice-versa.
+-- Trigger que seta o status da ocorrência (Entrada = 'E' ou Saída = 'S') 
+-- de acordo com o status da última ocorrência de um determinado usuário.
+-- Se a última ocorrência for entrada setar como saída e vice-versa.
 
-DELIMITER$$
+USE `db_rfid`;
 
-CREATE OR REPLACE TRIGGER seta_status
-    AFTER INSERT
+DELIMITER $$
+
+CREATE OR REPLACE TRIGGER set_new_status
+    BEFORE INSERT
     ON tb_ocorrencia FOR EACH ROW
 BEGIN
     DECLARE st_oc CHAR(1);
 
-    SELECT st_ocorrencia
-    INTO st_oc
-    FROM tb_ocorrencia
-    WHERE id_cadastro = NEW.id_cadastro
-    ORDER BY TIMESTAMP(dt_ocorrencia,hr_ocorrencia) DESC
-    LIMIT 1
+    IF EXISTS (SELECT * FROM tb_ocorrencia
+               WHERE id_cadastro = NEW.id_cadastro) THEN
 
-    IF st_oc = 'E' THEN
-        SET NEW.st_ocorrencia = 'S'
+        SET @ocorrencia_exists = TRUE;
+        SET @new_id_ocorrencia = NEW.id_ocorrencia;
+        SET @curr_id_cadastro = NEW.id_cadastro;
+        
+        SELECT st_ocorrencia
+        INTO @curr_st_ocorrencia
+        FROM tb_ocorrencia
+        WHERE id_cadastro = NEW.id_cadastro
+        AND id_dispositivo = NEW.id_dispositivo
+        ORDER BY TIMESTAMP(dt_ocorrencia,hr_ocorrencia) DESC
+        LIMIT 1;
     ELSE
-        SET NEW.st_ocorrencia = 'E'
+        set @ocorrencia_exists = FALSE;
+    END IF;
 END$$
 
-DELIMITER;
+CREATE OR REPLACE TRIGGER upd_ocorrencia_status
+    BEFORE INSERT
+    ON tb_ocorrencia FOR EACH ROW
+    FOLLOWS set_new_status
+BEGIN
+    IF @ocorrencia_exists = TRUE THEN
+        IF @curr_st_ocorrencia = 'E' THEN
+            SET NEW.st_ocorrencia = 'S';
+        ELSE
+            SET NEW.st_ocorrencia = 'E';
+        END IF;
+    END IF;
+END$$
+
+DELIMITER ;
+
+
