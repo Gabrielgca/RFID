7d544908f4e954b47cc3fc0387cc491ef2203b7b
SauloSCorrea <65734343+SauloSCorrea@users.noreply.github.com>
2020-07-29 18:05:07 -0300

    Alterações nos scripts e arquivos

--- Servidor/db_mapped_objects.py
+++ Servidor/db_mapped_objects.py
@@ -33,7 +33,54 @@ class Cadastro (Base):
     __tablename__ = "tb_cadastro"
     idCadastro = Column("id_cadastro",Integer, nullable=False, primary_key=True)
     noUsuario = Column("no_usuario",String(45), nullable=False)
-    cdCartao = Column("cd_cartao",String(25), nullable=False)
+
+    def __repr__(self):
+        return """<Cadastro
+    (id_cadastro='%d',
+     no_usuario='%s')>""" % (self.idCadastro, self.noUsuario)
+
+    def getDict(self):
+        self.dictionary = {}
+        self.dictionary['idCadastro'] = self.idCadastro
+        self.dictionary['noUsuario'] = self.noUsuario
+
+class Cartao(Base):
+    __tablename__ = "tb_cartao"
+    idCartao = Column("id_cartao",Integer,nullable=False,primary_key=True)
+    noCartao = Column("no_cartao",String(10),nullable=False)
+    def __repr__(self):
+        return """<Cartao
+    (id_cartao='%d',
+     no_cartao='%s')>""" % (self.idCartao, self.noCartao)
+
+    def getDict(self):
+        self.dictionary = {}
+        self.dictionary['idCartao'] = self.idCartao
+        self.dictionary['noCartao'] = self.noCartao
+
+class CadastroCartao(Base):
+    __tablename__ = "tb_cadastro_cartao"
+    idCadastroCartao = Column("id_cadastro_cartao",Integer,nullable=False,primary_key=True)
+    idCadastro = Column("id_cadastro",Integer,ForeignKey('tb_cadastro.id_cadastro'),nullable=False)
+    idCartao = Column("id_cartao",Integer,ForeignKey('tb_cartao.id_cartao'),nullable=False)
+    stEstado = Column("st_estado",String(1),nullable = False,default = 'A')
+
+    cadastro = relationship(Cadastro, back_populates = "cadastros_cartoes")
+    cartao = relationship(Cartao, back_populates = "cadastros_cartoes")
+
+    def __repr__(self):
+        return """<CadastroCartao
+    (id_cadastro_cartao='%d',
+     id_cadastro='%d',
+     id_cartao='%d',
+     st_estado='%s')>""" % (self.idCadastroCartao, self.idCadastro,
+                            self.idCartao,self.stEstado)
+
+    def getDict(self):
+        self.dictionary = {}
+        self.dictionary['idCadastroCartao'] = self.idCadastroCartao
+        self.dictionary['idCadastro'] = self.idCadastro
+        self.dictionary['idCartao'] = self.idCartao
 
 class Ocorrencia (Base):
     __tablename__ = "tb_ocorrencia"
@@ -58,5 +105,16 @@ class Ocorrencia (Base):
                                     self.idCadastro, self.dtOcorrencia, 
                                     self.hrOcorrencia,self.stOcorrencia)
 
+    def getDict(self):
+        self.dictionary = {}
+        self.dictionary['idOcorrencia'] = self.idOcorrencia
+        self.dictionary['idDispositivo'] = self.idDispositivo
+        self.dictionary['idCadastro'] = self.idCadastro
+        self.dictionary['dtOcorrencia'] = self.dtOcorrencia
+        self.dictionary['hrOcorrencia'] = self.hrOcorrencia
+        self.dictionary['stOcorrencia'] = self.stOcorrencia
+
 Dispositivo.ocorrencias = relationship("Ocorrencia", back_populates="dispositivo")
 Cadastro.ocorrencias = relationship("Ocorrencia", back_populates="cadastro")
+Cadastro.cadastros_cartoes = relationship("CadastroCartao", back_populates="cadastro")
+Cartao.cadastros_cartoes = relationship("CadastroCartao", back_populates="cartao")
--- Servidor/insere_dados_teste.py
+++ Servidor/insere_dados_teste.py
@@ -44,6 +44,8 @@ class InsereDados():
         self.session = parent.session
         self.dp = aliased(Dispositivo,name="dp")
         self.cd = aliased(Cadastro,name="cd")
+        self.ct = aliased(Cartao,name="ct")
+        self.cdct = aliased(CadastroCartao,name="cdct")
         self.oc = aliased(Ocorrencia,name="oc")
 
     def insertDispositivos (self):
@@ -67,14 +69,38 @@ class InsereDados():
             self.session.add(cadastro)
         self.session.commit()
 
+    def insereCartoes (self,cartoes):
+        for cartao in cartoes:
+            self.session.add(cartao)
+        self.session.commit()
+
+    def insereCadastrosCartoes(self,cadastrosCartoes):
+        for cadastroCartao in cadastrosCartoes:
+            newCadastroCartao = CadastroCartao(idCadastro = int(cadastroCartao[0]),
+                                               idCartao = int(cadastroCartao[1]),
+                                               stEstado = 'A')
+            self.session.add(newCadastroCartao)
+        self.session.commit()
+
     def selectAllDisps(self):
         dp = self.dp
         return self.session.query(dp,dp.idDispositivo,dp.noDispositivo)
+    
+    def selectAllCadastrosIds(self):
+        cd = self.cd
+        return self.session.query(cd.idCadastro)\
+                           .order_by(asc(cd.idCadastro)).all()
+
+    def selectAllCartoesIds(self):
+        ct = self.ct
+        return self.session.query(ct.idCartao)\
+                           .order_by(asc(ct.idCartao)).all()
+                           
 
 def generateNomes (nomes,sobrenomes):
     i = 0
-    for x in nnoArr:
-        for y in snoArr:
+    for x in nomes:
+        for y in sobrenomes:
             nomeCompleto = np.array([x,y])
             #print(nome)
             if i < 1:
@@ -98,19 +124,46 @@ def generateCards (qtdeDigitos,qtdeRegistros):
             i -= 1
     return cards
 
-def generateCadastros (nomes,cards,qtde):
+def generateCadastros (nomes,qtde):
     cadastros = []
     for i in range(0,qtde):
         currRand = random.randint(qtde - i)
         nnoUsua = nomes[currRand][0]
         snoUsua = nomes[currRand][1]
         nomes = np.delete(nomes,currRand,0)
-        cardUsua = cards[currRand]
-        cards = np.delete(cards,currRand)
-        cadastro = Cadastro(noUsuario = str(nnoUsua)+' '+str(snoUsua),
-                            cdCartao = str(cardUsua))
+        cadastro = Cadastro(noUsuario = str(nnoUsua)+' '+str(snoUsua))
         cadastros.append(cadastro)
     return cadastros
+
+def generateCartoes(cards,qtde):
+    cartoes = []
+    for i in range(0,qtde):
+        currRand = random.randint(qtde - i)
+        card = cards[currRand]
+        cards = np.delete(cards,currRand)
+        cartao = Cartao(noCartao = card) 
+        cartoes.append(cartao)
+    return cartoes
+
+def generateCadastroCartaoArr(qtde,cadastrosIds,cartoesIds):
+    cartoesIds = np.array(cartoesIds)
+    cartoesIds = cartoesIds.reshape(-1)
+    cadastrosIds = np.array(cadastrosIds)
+    cadastrosIds = cadastrosIds.reshape(-1)
+    i = 0
+
+    for cadastroId in cadastrosIds:
+        randCartaoId = np.random.choice(cartoesIds)
+        newArray = np.array([cadastroId,randCartaoId])
+        if i < 1:
+            cadastrosCartoes = newArray
+            cartoesIds = np.delete(cartoesIds,np.where(cartoesIds == randCartaoId))
+        else:
+            cadastrosCartoes = np.concatenate((cadastrosCartoes,newArray))
+            cartoesIds = np.delete(cartoesIds,np.where(cartoesIds == randCartaoId))
+        i += 1
+    cadastrosCartoes = cadastrosCartoes.reshape(qtde,2)
+    return cadastrosCartoes
 
 dbRfid = DbControl()
 
@@ -126,7 +179,6 @@ sel = CarregaDadosAux(dbAux)
 
 ins.insertDispositivos()
 
-'''
 nomes = sel.selectNomes()
 sobrenomes = sel.selectSobrenomes()
 
@@ -140,27 +192,36 @@ snoArr = snoArr.reshape(-1)
 
 nomesArr = generateNomes(nnoArr,snoArr)
 
-cards = generateCards(8,200)
+cardsArr = generateCards(8,200)
 
-cadastros = generateCadastros(nomesArr,cards,200)
+cadastros = generateCadastros(nomesArr,200)
 
 ins.insereCadastros(cadastros)
 
+cartoes = generateCartoes(cardsArr,200)
+
+ins.insereCartoes(cartoes)
+
+cadastrosId = ins.selectAllCadastrosIds()
+
+cartoesId = ins.selectAllCartoesIds()
+
+cadastrosCartoes = generateCadastroCartaoArr(200,cadastrosId,cartoesId)
+
+ins.insereCadastrosCartoes(cadastrosCartoes)
 #print(nnoArr)
 
 #print(snoArr)
 '''
-
-
-"""
 res = ins.selectAllDisps()
 #print(vars(res[0]))
-dictList = {}
-dictList['Dispositivos'] = []
+dicti = {}
+dictHeader = 'Dispositivos'
 arr = np.array(res.all())
 print(arr.ndim)
 for dit in res.column_descriptions:
     print(dit['name'])
+
 for row in res:
     i = 0
     if arr.ndim > 1:
@@ -172,16 +233,14 @@ for row in res:
                 #print("Objeto detectado")
                 objDict = col.getDict()
                 for key in col.getDict():           
-                    newDict[key] = objDict[key]
+                    dicti[dictHeader][key] = objDict[key]
                 i += 1
-                dictList['Dispositivos'].append(newDict)
             else:
                 z = 0
                 for dicti in res.column_descriptions:       
                     if z == i:
-                        newDict[dicti['name']] = col
+                        dicti[dictHeader][dicti['name']] = col
                         i += 1
-                        dictList['Dispositivos'].append(newDict)
                     z += 1
     else:
         newDict = {}
@@ -191,22 +250,20 @@ for row in res:
             #print("Objeto detectado")
             objDict = row.getDict()
             for key in row.getDict():           
-                newDict[key] = objDict[key]
+                dicti[dictHeader][key] = objDict[key]
             i += 1
-            dictList['Dispositivos'].append(newDict)
         else:
             z = 0
-            for dicti in res.column_descriptions:
+            for dictio in res.column_descriptions:
                 if z == i:       
-                    newDict[dicti['name']] = row[i]
+                    dicti[dictHeader][dictio['name']] = row[i]
                     i += 1
-                    dictList['Dispositivos'].append(newDict)
                 z += 1
-#print(dictList)
+#print(dicti)
 
-js = json.dumps(dictList,indent=4,separators=(',',':'))
+js = json.dumps(dicti,indent=4,separators=(',',':'))
 
 f = open("TesteDict2.txt","w")
 f.write(js)
 f.close
-"""
+'''
