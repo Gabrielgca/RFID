ad38e3a7a90dd342b8bbf5315fb86dca2187cbe5
SauloSCorrea <65734343+SauloSCorrea@users.noreply.github.com>
2020-08-19 11:45:12 -0300

    Novo db_control

--- Servidor/db_control.py
+++ Servidor/db_control.py
@@ -1,37 +1,56 @@
-#BIBLIOTECA - db_control.py
-#   Objetivo: realizar o controle da comunicação com banco de dados MySql
-#             usando o orm SQLAlchemy
-#   Importa: SQLAlchemy
-
-#Importando o SQLAlchemy e os objetos necessários
 import sqlalchemy
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine,ForeignKey
-from sqlalchemy import Column,Integer,Numeric,String,Date,Time,TIMESTAMP
+from sqlalchemy import Column,Integer,Boolean,SmallInteger,Numeric,String,Date,DateTime,Time,TIMESTAMP
 from sqlalchemy import between,func,text,asc,desc,case
 from sqlalchemy.orm import sessionmaker,relationship,aliased
 from sqlalchemy.sql.functions import Function
 from sqlalchemy.exc import SQLAlchemyError
-from sqlalchemy.ext.declarative.api import DeclarativeMeta
+from contextlib import contextmanager
 
 #Criando objeto de controle do banco de dados
-class DbControl:          
-    def __init__(self,engine = None,session = None):
-        self.engine = engine
-        self.session = session
+class DbControl: 
+	def __init__(self,engine = None,session = None):
+		self.engine = engine
+		self.session = session
 
-    def startSession (self):
-        Session = sessionmaker()
-        Session.configure(bind = self.engine)
-        self.session = Session()
+	def initSession (self):
+		self.Session = sessionmaker()
+		self.Session.configure(bind = self.engine)
 
-    def dbConnTest (self):
-        try:
-            conn = self.engine.connect()
-        except SQLAlchemyError as err:
-            raise Exception("Erro ao se conectar com o banco de dados!")
+	@contextmanager
+	def sessionQueryScope (self):
+		self.session = self.Session()
+		try:
+			yield self.session
+		except:
+			self.session.rollback()
+			raise
+		finally:
+			self.session.close()
 
-    def dbInit(self,url):
-        self.engine = create_engine("mysql+mysqlconnector://"+url)
-        self.dbConnTest()
-        self.startSession()
+	@contextmanager
+	def sessionTransactionScope (self,refresh = None):
+		self.session = self.Session()
+		try:
+			yield self.session
+			self.session.commit()
+			if refresh is not None:
+				self.session.refresh(refresh)
+		except:
+			self.session.rollback()
+			raise
+		finally:
+			self.session.close()
+			print('Seção encerrada!')
+
+	def dbConnTest (self):
+		try:
+			conn = self.engine.connect()
+		except SQLAlchemyError as err:
+			raise Exception('Erro ao se conectar com o banco de dados!')
+
+	def dbInit(self,url):
+		self.engine = create_engine('mysql+mysqlconnector://'+url)
+		self.dbConnTest()
+		self.initSession()
