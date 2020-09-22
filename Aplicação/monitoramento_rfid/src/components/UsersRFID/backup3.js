98badb24f89ab798532462cc2d7d7533d3b1aa0d
Silvio Júnior <silvio_junior96@hotmail.com>
2020-09-22 16:30:17 -0300

    Correção UsersRFID

--- Aplicação/monitoramento_rfid/src/components/UsersRFID/backup3.js
+++ Aplicação/monitoramento_rfid/src/components/UsersRFID/backup3.js
@@ -3,8 +3,6 @@ import { Link, withRouter } from 'react-router-dom';
 import firebase from '../../firebase';
 import FlatList from 'flatlist-react';
 import Loader from 'react-loader-spinner';
-import axios from 'axios';
-import baseURL from '../../service';
 import './UsersRFID.css';
 
 //pesquisa
@@ -13,6 +11,8 @@ import InputBase from '@material-ui/core/InputBase';
 import IconButton from '@material-ui/core/IconButton';
 import SearchIcon from '@material-ui/icons/Search';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+
+
 //modal
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
@@ -29,35 +29,50 @@ import Input from '@material-ui/core/Input';
 import AddIcon from '@material-ui/icons/Add';
 import ClearIcon from '@material-ui/icons/Clear';
 import EditIcon from '@material-ui/icons/Edit';
+
+
+
 class UsersRFID extends Component {
+
     constructor(props) {
         super(props);
         this.state = {
             filterUsers: [],
             filter: '',
-            selectedUser: { key: '', name: '', idade: '', cargo: '', status: '', rfid: '' },
+            selectedUser: { key: '', name: '', idade: '', cargo: '', status: '' },
             nome: localStorage.nome,
             cargo: localStorage.cargo,
             modalDeactivateOpen: false,
             modalReactivateOpen: false,
-            people: []
+            people: [],
+            selectedPerson: {
+                name: "",
+                idade: '',
+                cargo: '',
+                status: "",
+                rfid: ''
+            }
         };
 
         this.logout = this.logout.bind(this);
     }
+
     async componentDidMount() {
         if (!firebase.getCurrent()) {
             this.props.history.replace('/login');
             return null;
         }
+
         firebase.getUserName((info) => {
             localStorage.nome = info.val().nome;
             this.setState({ nome: localStorage.nome });
         });
+
         firebase.getUserPerfil((info) => {
             localStorage.cargo = info.val().cargo;
             this.setState({ cargo: localStorage.cargo });
         });
+
         /* if (this.state.cargo === "Auxiliar") {
             this.props.history.replace("/dashboard");
             return null;
@@ -66,17 +81,15 @@ class UsersRFID extends Component {
             alert("Acesso autorizado");
             alert(this.state.cargo)
         } */
-        this.getUsersRFID();
 
     }
 
     modalOpen = async (user) => {
         this.setState({
-            selectedUser: {
-                key: user.id_user,
-                name: user.nome,
+            selectedPerson: {
+                name: user.name,
                 status: user.status,
-                rfid: user.RFID,
+                rfid: user.rfid,
                 idade: user.idade,
                 cargo: user.cargo
             }
@@ -86,8 +99,6 @@ class UsersRFID extends Component {
 
     modalClose = async () => {
         this.setState({ modalOpen: false });
-        this.getUsersRFID();
-        this.setState({ selectedUser: { key: '', name: '', idade: '', cargo: '', status: '', rfid: '' } })
     }
 
     logout = async () => {
@@ -97,44 +108,43 @@ class UsersRFID extends Component {
             });
         localStorage.removeItem("nome");
         this.props.history.push('/');
+
     }
 
-    handleNameChange = async (event) => {
-        let newName = this.state.selectedUser;
+    handleNameChange = async (event) => { // Não utilizada
+        let newName = this.state.selectedPerson;
         newName.name = event.target.value;
-        this.setState({ selectedUser: newName })
+        this.setState({ selectedPerson: newName })
     }
 
-    handleStatusChange = async (event) => {
-        let newSelectedPerson = this.state.selectedUser;
+    handleStatusChange = async (event) => { // Não utilizada
+        let newSelectedPerson = this.state.selectedPerson;
         newSelectedPerson.status = event.target.value;
         this.setState({ selectedPerson: newSelectedPerson })
-        this.setState({ selectedUser: newSelectedPerson })
     }
 
     handleOfficeChange = async (event) => {
-        let newJob = this.state.selectedUser;
+        let newJob = this.state.selectedPerson;
         newJob.cargo = event.target.value;
-        this.setState({ selectedUser: newJob })
+        this.setState({ selectedPerson: newJob })
     }
 
     handleSaveUpdateUser = async () => {
         alert(JSON.stringify(this.state.selectedPerson))
     }
 
-
     searchUser = () => {
         this.state.filterUsers = [];
         this.state.people.forEach((user) => {
+
             if (user.name.toUpperCase().includes(this.state.filter.toUpperCase()) || user.name.toUpperCase() == this.state.filter.toUpperCase()) {
 
                 let list = {
-                    key: user.id_user,
-                    name: user.nome,
+                    name: user.name,
                     idade: user.idade,
                     cargo: user.cargo,
                     status: user.status,
-                    rfid: user.RFID
+                    rfid: user.rfid
                 }
 
                 let array = this.state.filterUsers;
@@ -146,7 +156,7 @@ class UsersRFID extends Component {
 
     getUsers = () => {
         this.setState({ users: [] });
-        this.state.selectedUser.map((allUsers) => {
+        this.state.selectedPerson.map((allUsers) => {
             allUsers.forEach((oneUser) => {
                 let list = {
                     key: oneUser.key,
@@ -159,82 +169,52 @@ class UsersRFID extends Component {
             })
         })
     }
+
     reactivateUser = () => {
         alert('usuario reativado');
         this.handleCloseReactivateUser();
         this.getUsers();
     }
+
     handleCloseReactivateUser = () => {
         this.setState({ modalReactivateOpen: false });
         this.setState({ selectedUser: { key: '', name: '', idade: '', cargo: '', status: '', rfid: '' } })
     }
+
     handleClearFilter = () => {
         this.setState({ filterUsers: [] });
         this.setState({ filter: '' });
     }
+
     handleDeactivateUser = (user) => {
         this.setState({ selectedUser: { key: user.key, name: user.name, status: user.status, idade: user.idade, cargo: user.cargo, rfid: user.rfid } })
         this.setState({ modalDeactivateOpen: true })
     }
+
     handleCloseDeactivateUser = () => {
         this.setState({ modalOpen: false });
         this.setState({ selectedUser: { key: '', name: '', idade: '', cargo: '', status: '', rfid: '' } })
     }
+
     handleReactivate = (user) => {
         this.setState({ selectedUser: { key: user.key, name: user.name, status: user.status, status: user.status, cargo: user.cargo, rfid: user.rfid } })
         this.setState({ modalReactivateOpen: true })
     }
+
     deactivateUser = async (key) => {
         alert('usuario desativado')
         this.handleCloseDeactivateUser();
         this.getUsers();
     }
+
     handleCloseDeactivate = () => {
         this.setState({ modalDeactivateOpen: false });
         this.setState({ selectedUser: { key: '', name: '', idade: '', cargo: '', status: '', rfid: '' } })
     }
 
-    getUsersRFID = async () => {
-        await axios.get(baseURL + 'userInfo')
-            .then(response => {
-                this.setState({ people: response.data.usuarios })
-            })
-            .catch(error => {
-                alert('Error: ' + JSON.stringify(error))
-            })
-    }
-
-    handleUpdateUser = async () => {
-        if (this.state.selectedUser.nome !== '' &&
-            this.state.selectedUser.cargo !== '' &&
-            this.state.selectedUser.status !== '') {
-            let params = {
-                id_user: this.state.selectedUser.key,
-                RFID: this.state.selectedUser.rfid,
-                nome: this.state.selectedUser.name,
-                idade: this.state.selectedUser.idade,
-                cargo: this.state.selectedUser.cargo,
-                status: this.state.selectedUser.status
-
-            }
-            //alert(JSON.stringify(params))
-
-            await axios.post(baseURL + 'updateUser', params)
-                .then(response => {
-                    alert(JSON.stringify(response.data))
-                    console.log(response)
-                    this.modalClose();
-                })
-                .catch(error => {
-                    console.log(error);
-
-                })
-
-        }
-    }
-
 
     render() {
+        const { people } = this.state;
         return (
             <div className="container">
                 <header id="new">
@@ -254,9 +234,11 @@ class UsersRFID extends Component {
                     <IconButton type="button" onClick={this.searchUser}>
                         <SearchIcon />
                     </IconButton>
+
                     <IconButton type="button" onClick={this.handleClearFilter}>
                         <ClearIcon />
                     </IconButton>
+
                     <IconButton type="button" onClick={() => { this.props.history.push("/usersRFID/new") }}>
                         <AddIcon style={{ color: 'green' }} />
                     </IconButton>
@@ -264,16 +246,15 @@ class UsersRFID extends Component {
                 <FlatList
                     list={this.state.filterUsers.length > 0 ? this.state.filterUsers : this.state.people}
                     renderItem={(item) => (
-                        <div className={item.status === "A" ? "users-rfid-card" : "users-rfid-card-disabled"}>
-                            <p><b>Nome:</b> {item.nome}</p>
+                        <div className={item.status === "Ativo" ? "users-rfid-card" : "users-rfid-card-disabled"}>
+                            <p><b>Nome:</b> {item.name}</p>
                             <p><b>Idade:</b> {item.idade}</p>
                             <p><b>Cargo:</b> {item.cargo}</p>
-                            {item.status === 'A' ? (<p><b>Status:</b> Ativo</p>) : (<p></p>)}
-                            {item.status === 'I' ? (<p><b>Status:</b> Inativo</p>) : (<p></p>)}
-                            <p><b>RFID:</b> {item.RFID}</p>
+                            <p><b>Status:</b> {item.status}</p>
+                            <p><b>RFID:</b> {item.rfid}</p>
                             <div className="btnArea">
                                 <Button endIcon={<EditIcon />} onClick={() => { this.modalOpen(item) }} style={{ backgroundColor: 'green', color: '#FFF', marginRight: 10 }}>Editar</Button>
-                                {item.status == 'A' ? (
+                                {item.status == 'Ativo' ? (
                                     <Button endIcon={<EditIcon />} onClick={() => { this.handleDeactivateUser(item) }} style={{ backgroundColor: 'red', color: '#FFF' }}>Desativar</Button>
                                 ) : (
                                         <Button endIcon={<EditIcon />} onClick={() => { this.handleReactivate(item) }} style={{ backgroundColor: 'blue', color: '#FFF' }}>Reativar</Button>
@@ -282,9 +263,7 @@ class UsersRFID extends Component {
                             </div>
                         </div>
 
-
                     )}
-                    /* sorchBy={['status', 'cargo', 'nome']} */
                     renderWhenEmpty={() => (
                         <div className="div-loader">
                             <Loader
@@ -294,6 +273,7 @@ class UsersRFID extends Component {
                                 height={100}
                                 width={100}
                             //timeout={3000} //3 secs
+
                             />
                         </div>)}
                 />
@@ -305,6 +285,7 @@ class UsersRFID extends Component {
                     onClose={this.modalClose}
                     aria-labelledby="alert-dialog-title"
                     aria-aria-describedby="alert-dialog-description"
+
                 >
                     <DialogTitle id="alert-dialog-title">{"Detalhes do usuário"}</DialogTitle>
                     <DialogContent>
@@ -312,72 +293,74 @@ class UsersRFID extends Component {
                             <TextField
                                 autoFocus
                                 margin='dense'
+                                id='name'
                                 label='Nome'
                                 type='text'
-                                value={this.state.selectedUser.name}
+                                value={this.state.selectedPerson.name}
                                 onChange={this.handleNameChange}
                                 fullWidth
 
                             />
                             <FormControl disabled>
                                 <InputLabel>Idade</InputLabel>
-                                <Input value={this.state.selectedUser.idade} />
+                                <Input value={this.state.selectedPerson.idade} />
                             </FormControl>
-
-                            <TextField
-                                autoFocus
-                                margin='dense'
-                                label='Função'
-                                type='text'
-                                value={this.state.selectedUser.cargo}
-                                onChange={this.handleOfficeChange}
-                                fullWidth
-
-                            />
+                            <InputLabel className="selectLabel">Cargo</InputLabel>
+                            <Select
+                                value={this.state.selectedPerson.cargo}
+                                onChange={this.handleCargoChange}
+                            >
+                                <MenuItem value="Analista">Analista</MenuItem>
+                                <MenuItem value="Administrador">Administrador</MenuItem>
+                                <MenuItem value="Operador">Operador</MenuItem>
+                            </Select>
                             <InputLabel className="selectLabel">Status</InputLabel>
                             <Select
-                                value={this.state.selectedUser.status}
+                                value={this.state.selectedPerson.status}
                                 onChange={this.handleStatusChange}
                             >
-                                <MenuItem value="A">Ativo</MenuItem>
-                                <MenuItem value="I">Inativo</MenuItem>
+                                <MenuItem value="Ativo">Ativo</MenuItem>
+                                <MenuItem value="Inativo">Inativo</MenuItem>
                             </Select>
 
                             <FormControl disabled>
                                 <InputLabel>RFID</InputLabel>
-                                <Input value={this.state.selectedUser.rfid} />
+                                <Input value={this.state.selectedPerson.rfid} />
                             </FormControl>
                         </DialogContentText>
                     </DialogContent>
                     <DialogActions>
-                        <Button onClick={this.handleUpdateUser} autoFocus style={{ backgroundColor: 'green', color: '#FFF' }}>Salvar</Button>
+                        <Button onClick={this.handleSaveUpdateUser} autoFocus style={{ backgroundColor: 'green', color: '#FFF' }}>Salvar</Button>
                         <Button onClick={this.modalClose} style={{ backgroundColor: 'red', color: '#FFF' }} autoFocus>
                             Cancelar
                         </Button>
                     </DialogActions>
                 </Dialog>
+
+
                 {/* Desativar usuário */}
                 <Dialog open={this.state.modalDeactivateOpen} onClose={this.handleDeactivateUser} arial-label-title="form-dialog-title">
                     <DialogTitle id='form-dialog-title'>Desetivar Usuário</DialogTitle>
                     <DialogContent>
                         <DialogContentText>
-                            Tem certeza que deseja desativar o usuário <b>{this.state.selectedUser.name} </b>?
+                            Tem certeza que deseja desativar o usuário <b>{this.state.selectedPerson.name} </b>?
                     </DialogContentText>
                     </DialogContent>
 
 
                     <DialogActions>
-                        <Button onClick={() => { this.deactivateUser(this.state.selectedUser.name) }} style={{ backgroundColor: 'green', color: '#FFF' }}>Sim</Button>
+                        <Button onClick={() => { this.deactivateUser(this.state.selectedPerson.name) }} style={{ backgroundColor: 'green', color: '#FFF' }}>Sim</Button>
                         <Button onClick={this.handleCloseDeactivate} style={{ backgroundColor: 'red', color: '#FFF' }}>Cancelar</Button>
                     </DialogActions>
                 </Dialog>
+
                 {/* Reativar usuario */}
                 <Dialog open={this.state.modalReactivateOpen} onClose={this.handleReactivate} arial-label-title='form-ialog-title'>
                     <DialogTitle>
                         Reativar Usuário
                     </DialogTitle>
                     <DialogContent>
-                        <DialogContentText>Deseja reativar usuário <b>{this.state.selectedUser.name}</b>?</DialogContentText>
+                        <DialogContentText>Deseja reativar usuário <b>{this.state.selectedPerson.name}</b>?</DialogContentText>
                     </DialogContent>
                     <DialogActions>
                         <Button onClick={() => { this.reactivateUser(this.state.selectedUser.key) }} style={{ backgroundColor: 'green', color: '#FFF' }}>Sim</Button>
@@ -388,4 +371,7 @@ class UsersRFID extends Component {
         );
     }
 }
+
+
+
 export default withRouter(UsersRFID);
